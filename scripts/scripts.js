ce78823e57865f8ebc9f5bf6125f55b3ee2543d3
Juozas Kaziukenas <juozas@juokaz.com>
2011-07-12 14:59:28 +0100

    Correct slides spacing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,8 @@
 function fixParameters (){
-    var header = 50;
     $(".slide").each(function(){
 	    if($(this).outerHeight() < $(window).height()){
-		    var itemMargin = ($(window).height() - header - $(this).outerHeight()) /2;
-		    $(this).css('margin-top', header + itemMargin+'px');
+		    var itemMargin = ($(window).height() - $(this).outerHeight()) /2;
+		    $(this).css('margin-top', itemMargin+'px');
 	    }
     });
 };
