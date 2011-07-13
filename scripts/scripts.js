7f045e39adf295f4189488d823f4ccd3c9c668da
Juozas Kaziukenas <juozas@juokaz.com>
2011-07-13 02:09:57 +0100

    Initial index background image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,8 @@ function fixParameters (){
     $(".slide").each(function(){
 	    if($(this).outerHeight() < $(window).height()){
 		    var itemMargin = ($(window).height() - $(this).outerHeight()) /2;
-		    $(this).css('margin-top', itemMargin+'px');
+		    $(this).css('padding-top', itemMargin+'px');
+		    $(this).css('padding-bottom', itemMargin+'px');
 	    }
     });
 };
